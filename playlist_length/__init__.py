8239a41a39a23dc2b2e2f1a4a587b0aa76b4e6fb
libcolor <97296222+libcolor@users.noreply.github.com>
2022-11-11 11:34:46 +0000

    tracing: Use stack of calling function for stack tracer

--- playlist_length/__init__.py
+++ playlist_length/__init__.py
@@ -4,3 +4,4 @@ from .__version__ import __version__
 __version__ = __version__
 __author__ = 'Karan Suthar, Mohit Solanki'
 __license__ = 'MIT License'
+__description__ = 'A command-line tool to calculate the length of videos in a directory'
