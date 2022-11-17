c8042eed38492455fc003b2e1416d7f0bcd3f4de
libcolor <97296222+libcolor@users.noreply.github.com>
2022-11-17 13:50:45 +0000

    Ameba upgrade

--- playlist_length/main.py
+++ playlist_length/main.py
@@ -11,3 +11,7 @@ from concurrent.futures import ProcessPoolExecutor
 
 import magic
 from huepy import bold, green, red
+from tqdm import tqdm
+
+from playlist_length.utils import pluralize, CacheUtil
+from playlist_length.__version__ import __version__
