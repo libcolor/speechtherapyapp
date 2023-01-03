fdafb8645b8874de194d9abc2bfc51cba7241171
libcolor <97296222+libcolor@users.noreply.github.com>
2023-01-03 20:16:02 +0000

    Update tests.

--- playlist_length/main.py
+++ playlist_length/main.py
@@ -132,3 +132,5 @@ def calculate_length(BASE_PATH, no_subdir, media_type, queue, cache_ob):
         minutes_string = pluralize(minutes, base='minute', suffix='s')
         result = 'Length of all {} is {} and {}.'.format(
             media_type, hours_string, minutes_string
+        )
+    return bold(green(result))
