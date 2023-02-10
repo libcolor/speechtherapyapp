6c629fec0d5ca7f0122c8b0b99d5ecc4143fe8ed
libcolor <97296222+libcolor@users.noreply.github.com>
2023-02-10 05:32:38 +0000

    Test readme

--- playlist_length/utils.py
+++ playlist_length/utils.py
@@ -12,3 +12,7 @@ def pluralize(number, base, suffix):
     if number < 2:
         return '{} {}'.format(number, base)
     return '{} {}{}'.format(number, base, suffix)
+
+
+class CacheUtil:
+    def __init__(self, path, media_type):
